2c46aa3de770bdfe6ec408fa6b29a0cd1bb9068c
AlexDvor <alexmatvichuk@gmail.com>
2025-08-14 15:31:31 +0200

    Remove motion and added custom animation

--- src/js/scrollAnimation.js
+++ src/js/scrollAnimation.js
@@ -1,5 +1,3 @@
-import { animate } from 'motion';
-
 export function animateOnScroll(selector, options = {}) {
   const elements = document.querySelectorAll(selector);
 
@@ -7,14 +5,19 @@ export function animateOnScroll(selector, options = {}) {
     entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          animate(
-            entry.target,
+          entry.target.animate(
+            [
+              { opacity: 0, transform: 'translateY(50px)' },
+              { opacity: 1, transform: 'translateY(0px)' },
+            ],
             {
-              opacity: [0, 1],
-              transform: ['translateY(50px)', 'translateY(0px)'],
-            },
-            { duration: 0.8, easing: 'ease-out', ...options }
+              duration: 800,
+              easing: 'ease-out',
+              fill: 'forwards',
+              ...options,
+            }
           );
+
           entry.target.classList.remove('hidden');
           observer.unobserve(entry.target);
         }
